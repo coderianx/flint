33ca23a15622eda316ca312ce39bad77ba770da0
Coderian <grayvortexdev@gmail.com>
2025-08-24 17:39:46 +0300

    Update version.go

--- version.go
+++ version.go
@@ -5,16 +5,16 @@ import (
 )
 
 func Version() {
-	const Version = "v1.2.0"
+	const Version = "v1.3.0"
 	fmt.Println("Flint Version:", Version)
 	fmt.Println("https://github.com/coderianx/flint")
 }
 
 func Info() {
-	const Version = "v1.2.0"
+	const Version = "v1.3.0"
 	const License = "MIT"
-	const Author = "CoderianX"
-	const Release_date = "2025/08/12"
+	const Author = "Coderian"
+	const Release_date = "2025/08/24"
 	fmt.Println("========== Flint Info ==========")
 	fmt.Println("Flint Version:", Version)
 	fmt.Println("License:", License)
