240a6809b2b8c2e24a43d234a994550aa25f504e
Coderian <thecoderian.dev@gmail.com>
2025-09-23 23:18:40 +0300

    Update version.go

--- version.go
+++ version.go
@@ -5,16 +5,16 @@ import (
 )
 
 func Version() {
-	const Version = "v1.3.1"
+	const Version = "v1.4"
 	fmt.Println("Flint Version:", Version)
 	fmt.Println("https://github.com/coderianx/flint")
 }
 
 func Info() {
-	const Version = "v1.3.1"
+	const Version = "v1.4"
 	const License = "MIT"
 	const Author = "Coderian"
-	const Release_date = "2025/09/01"
+	const Release_date = "2025/09/24"
 	fmt.Println("========== Flint Info ==========")
 	fmt.Println("Flint Version:", Version)
 	fmt.Println("License:", License)
